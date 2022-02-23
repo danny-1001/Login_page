d36da48c08cf6d517bb0ed50375503391d3f010f
Your Name <shazam1258@gmail.com>
2022-02-23 14:38:29 -0500

    updated user interface, added container div inside form tag

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -11,64 +11,66 @@ function Main() {
               Use whenever you want, where ever you want, however you want.
             </p>
           </div>
-          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+          <div className="col-md-6">
             <form className="main_from">
-              <div className="form-outline mb-4">
-                <input
-                  type="email"
-                  id="email"
-                  className="form-control form-control-lg text-align-center align-items-center"
-                  placeholder="Enter a valid email address"
-                  required
-                />
-                <label className="form-label" for="email">
-                  Email address
-                </label>
-              </div>
-
-              <div className="form-outline mb-3">
-                <input
-                  type="password"
-                  id="password"
-                  className="form-control form-control-lg"
-                  placeholder="Enter password"
-                  required
-                />
-                <label className="form-label" for="password">
-                  Password
-                </label>
-              </div>
-
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="form-check mb-0">
+              <div className="container form_card">
+                <div className="form-outline mb-4">
                   <input
-                    className="form-check-input "
-                    type="checkbox"
-                    value=""
-                    id="checkbox"
+                    type="email"
+                    id="email"
+                    className="form-control form-control-lg text-align-center align-items-center"
+                    placeholder="Enter a valid email address"
+                    required
                   />
-                  <label className="form-check-label" for="checkbox">
-                    Remember me
+                  <label className="form-label" for="email">
+                    Email address
                   </label>
                 </div>
-                <a href="#!" className="text-body">
-                  Forgot password?
-                </a>
-              </div>
 
-              <div className="text-center text-lg-start mt-4 pt-2">
-                <button type="button" className="btn login_button">
-                  Login
-                </button>
-              </div>
+                <div className="form-outline mb-3">
+                  <input
+                    type="password"
+                    id="password"
+                    className="form-control form-control-lg"
+                    placeholder="Enter password"
+                    required
+                  />
+                  <label className="form-label" for="password">
+                    Password
+                  </label>
+                </div>
 
-              <div className="divider d-flex align-items-center my-4">
-                <p className="text-center fw-bold mx-3 mb-0">Or</p>
-              </div>
-              <div className="text-center text-lg-start mt-4 pt-2">
-                <button type="button" className="btn sign_up_button">
-                  Sign Up
-                </button>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="form-check mb-0">
+                    <input
+                      className="form-check-input "
+                      type="checkbox"
+                      value=""
+                      id="checkbox"
+                    />
+                    <label className="form-check-label" for="checkbox">
+                      Remember me
+                    </label>
+                  </div>
+                  <a href="#!" className="text-body">
+                    Forgot password?
+                  </a>
+                </div>
+
+                <div className="text-center text-lg-center mt-4 pt-2">
+                  <button type="button" className="btn login_button">
+                    Login
+                  </button>
+                </div>
+
+                <div className="divider d-flex align-items-center my-4">
+                  <p className="text-center fw-bold mx-3 mb-0">Or</p>
+                </div>
+                <div className="text-center text-lg-center mt-4 pt-2">
+                  <button type="button" className="btn sign_up_button">
+                    Sign Up
+                  </button>
+                </div>
               </div>
             </form>
           </div>
